e937dda85f0b87839dde64779ebee07f84321325
jgonzet <ignacio.gonzet@gmail.com>
2024-12-31 19:18:01 -0600

    Se agregaron variables pendientes al modelo

--- app2.py
+++ app2.py
@@ -8,33 +8,18 @@ client = OpenSearch(
     hosts=[{"host": "localhost", "port": 9200}],
     http_auth=('admin', '@Thinkpad57!')
 )
-
-def get_vector_by_id(query_id):
-    try:
-        # Realizar una consulta para obtener el documento correspondiente al ID
-        response = client.get(index="embedding_index", id=query_id)
-        
-        if response["found"]:
-            # Si se encuentra el documento, devuelve el vector
-            return response['hits']['hits']  # Asumiendo que el campo se llama 'embedding'
-        else:
-            return None
-    except Exception as e:
-        print(f"Error al obtener el vector por ID: {str(e)}")
-        return None
     
 
 @app.route("/", methods=["GET"])
 def home():
     return jsonify({"message": "API para consultas a OpenSearch"}), 200
 
-@app.route("/search", methods=["POST"])
+@app.route("/search_movie", methods=["POST"])
 def search():
     try:
         print("Solicitud recibida")  # Mensaje para confirmar la llegada de la solicitud
         data = request.json
         
-        # Recibir los parámetros ID y K desde la solicitud
         query_id = data.get("ID")  # ID del vector
         k = data.get("K")
 
@@ -44,26 +29,30 @@ def search():
         if not isinstance(k, int) or k <= 0:
             return jsonify({"error": "El valor de K debe ser un número entero positivo"}), 400
 
-        # Obtener el vector correspondiente al ID de la base de datos (esto depende de cómo esté almacenado en OpenSearch)
-        query_vector = get_vector_by_id(query_id)  # Función para obtener el vector correspondiente al ID
-
-        if query_vector is None:
-            return jsonify({"error": "El ID proporcionado no existe en la base de datos"}), 404
-
-        # Realizar la búsqueda KNN en OpenSearch usando el vector y K
-        response = client.search(
-            index="embedding_index",
-            body={
+        # Query
+        try:
+            query = {
+                "size": k,
                 "query": {
                     "knn": {
-                        "embedding": {
-                            "vector": query_vector,
-                            "k": k
-                        }
+                    "vector": {
+                        "vector": movie_embeddings_matrix[query_id],
+                        "k" : k
+                    }
                     }
                 }
             }
-        )
+        
+        response = client.search(index="movie", id=query_id)
+        
+        if response["found"]:
+            # Si se encuentra el documento, devuelve el vector
+            return response['hits']['hits']  # Asumiendo que el campo se llama 'embedding'
+        else:
+            return None
+    except Exception as e:
+        print(f"Error al obtener el vector por ID: {str(e)}")
+        return None
 
         # Extraer los resultados
         results = [{"id": hit["_id"], "score": hit["_score"], "vector": hit["_source"]["embedding"]} for hit in response["hits"]["hits"]]
